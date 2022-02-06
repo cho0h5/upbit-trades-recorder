3e23b80d2fb2336eba90a342fa593cc1c8b9c700
Youngho Cho <dominoyh5@gmail.com>
2022-02-06 14:23:10 +0900

    add library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cho0h5/upbit-trades-recorder
 
 go 1.17
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,27 @@
-package min
+package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/gorilla/websocket"
 )
 
+const URL string = "wss://api.upbit.com/websocket/v1"
+const REQUEST string = `[{"ticket":"test"},{"type":"trade","codes":["KRW-XRP"]},{"format":"SIMPLE"}]`
+
 func main() {
-	fmt.Println("hi")
+	c, _, err := websocket.DefaultDialer.Dial(URL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.WriteMessage(websocket.TextMessage, []byte(REQUEST))
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(message))
+	}
 }
